58f62d589ef8286081053ce03f03420a5fc0c14e
Mubangizi <mubangizia22@gmail.com>
2020-01-14 12:51:18 +0300

    setup school columns

--- models/student.py
+++ models/student.py
@@ -1,6 +1,6 @@
 from app import db
 
-class Student(db.Model, ToDict):
+class Student(db.Model):
     """ Student table definition """
 
     _tablename_ = "student"
@@ -8,7 +8,7 @@ class Student(db.Model, ToDict):
     # fields of the Student table
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), nullable=False, default="")
-    aggregate = db.Column(db.Int(256), unique=True, nullable=False, default="")
+    aggregate = db.Column(db.Integer(100), nullable=False, default="")
 
     def __init__(self, name, aggregate):
         """ initialize with name and aggregate """
