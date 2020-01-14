22bf7aade48184fb5769eedd11b0bdae0d6bd547
Mubangizi <mubangizia22@gmail.com>
2020-01-14 12:40:35 +0300

    added student columns

--- models/student.py
+++ models/student.py
@@ -4,3 +4,14 @@ class Student(db.Model, ToDict):
     """ Student table definition """
 
     _tablename_ = "student"
+
+    # fields of the Student table
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256), nullable=False, default="")
+    aggregate = db.Column(db.Int(256), unique=True, nullable=False, default="")
+
+    def __init__(self, name, aggregate):
+        """ initialize with name and aggregate """
+        self.name = name
+        self.aggregate = aggregate
+        
