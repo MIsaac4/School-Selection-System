1d81d52aa64491d447238b1905c56b722c6df846
Mubangizi <mubangizia22@gmail.com>
2020-01-15 16:25:42 +0300

    changed student model

--- models/student.py
+++ models/student.py
@@ -1,6 +1,7 @@
 from app import db
 
 from models.parent import Parent
+from models.school import School
 
 class Student(db.Model, Parent):
     """ Student table definition """
@@ -11,6 +12,12 @@ class Student(db.Model, Parent):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), unique=True, nullable=False)
     aggregate = db.Column(db.Integer, nullable=False)
+    """ student choices """
+    first_choice = db.Column("first_choice", db.Integer, db.ForeignKey(School.id, ondelete='CASCADE'), nullable=False)
+    second_choice = db.Column("first_choice", db.Integer, db.ForeignKey(School.id, ondelete='CASCADE'), nullable=False)
+    third_choice = db.Column("first_choice", db.Integer, db.ForeignKey(School.id, ondelete='CASCADE'), nullable=False)        
+    fourth_choice = db.Column("first_choice", db.Integer, db.ForeignKey(School.id, ondelete='CASCADE'), nullable=False)
+    is_assigned = db.Column("is_assigned", db.Integer, default=0)
 
     def __init__(self, name, aggregate):
         """ initialize with name and aggregate """
